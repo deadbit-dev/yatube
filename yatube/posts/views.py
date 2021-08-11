4024ce59a1b787908dd702ba36aa157bb7c61cc8
Pavel Petrochenko <pavelpetrochenko2001@gmail.com>
2021-08-11 21:57:21 +0300

    Update system and removed static files

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,20 +1,20 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
 def index(request):
-    template = 'posts/index.html'
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.all()
     context = {
         'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)
 
 
-def group_posts(request):
-    template = 'posts/group_list.html'
-    text = 'Информация на странице группы будет тут.'
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = group.posts.all()
     context = {
-        'text': text,
+        'group': group,
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/group_list.html', context)
